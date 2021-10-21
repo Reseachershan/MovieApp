02fc4762f95db6a936d817d4462a733c4fd04b64
Muhammad Zeeshan <resercher.zeeshanjaved@gmail.com>
2021-10-21 14:27:52 +0500

    Initial commit

--- src/Movie.js
+++ src/Movie.js
@@ -4,21 +4,21 @@ const Movie = (props) => {
     return (
         <>
         
-        <div className="container-fluid   d-flex flex-nowrap   overflow-auto ">
-        {props.movie.map((movie,ind)=>{
+        <div className="container-fluid d-flex flex-nowrap   overflow-auto">
+        {props.movie.map((movie,index)=>{
             return(
         
              <>       
-          {/* <img  className="img-fluid d-flex flex-column m-1" src={movie.Poster} alt="movie image" /> */}
-          <div className="card col-12 m-1" style={{width:"18rem",backgroundColor:"black"}} >
-  <img src={movie.Poster} className="img-fluid img"  style={{backgroundColor:"wheat",maxWidth:"500px" ,height:"350px" }}     alt="..." />
+ <div className="card col-12 m-1" style={{width:"18rem",backgroundColor:"black"}} >
+  <img  src={movie.Poster} className="img-fluid img"  style={{backgroundColor:"wheat",width:"500px" ,height:"350px" }}     alt="..." />
   <div className="card-body" style={{fontFamily:"cursive" , color:"white" }}>
-    <h5 className="card-text"> Movie Name :{movie.Title}</h5>
+  <button type="button" className="btn btn-outline-danger link-btn mb-3 "><a href={"https://www.imdb.com/title/"+movie.imdbID} target="_blank">Watch Trailer</a></button>
+    <h5  className="card-text"> Movie Name :{movie.Title}</h5>
     <h5 className="card-text">Release Date :{movie.Year}</h5>
-    <h5 type="button" class="btn btn-outline-danger link-btn"><a href={"https://www.imdb.com/title/"+movie.imdbID} target="_blank">Watch Trailer</a></h5>
+    <br />
+     
   </div>
 </div>
-
 
           </>
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './logo.png';
 const Navbar =(props)=>{
 return(
     <>
     <nav className="navbar navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="http://localhost:3000/" style={{cursor:"pointer"}}>Movie</a>
+    <a  className="navbar-brand" href="http://localhost:3001/"><img className="mb-1 " style={{width:"25px"}} src={logo}/> Movie</a>
     <form className="d-flex">
       <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={props.change} />
 
